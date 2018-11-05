2d13e304b3564ad9102045cfc21388e60e60cac9
wujiayuan <2463764964@qq.com>
2018-11-05 09:32:21 +0800

    新增concat功能

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
+//gulp配置文件
 var gulp = require('gulp');
 var scss = require('gulp-sass');
-gulp.task('default', function() {
+var concat = require('gulp-concat');
+
+//创建gulp任务流，编译sass
+gulp.task('sassDev', function() {
     return gulp.src('src/scss/style.scss')
         .pipe(scss()).pipe(gulp.dest('src/css'))
 })
+
+//合并js/css
+gulp.task('concat', function() {
+    return gulp.src('src/css/style.css')
+        .pipe(concat()).pipe(gulp.dest('until/concat'))
+})
+
+//监听源文件
+// gulp.task('watch', function() {
+//     return gulp.watch('src/css/style.css')
+//         .pipe(scss())
+//         .pipe(gulp.dest('src/css'), gulp.series('sassDev'))
+// })
+
+//整体执行
+// gulp.task('dev', function() {
+//     return gulp.series('sassDev', 'concat', 'watch')
+// })
